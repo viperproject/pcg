33b817a4161945b28a206fc4f574a49d27435a82
Zack Grannan <zgrannan@protonmail.com>
2025-02-22 13:41:01 -0800

    Continue improvements

--- src/borrow_pcg/action/executed_actions.rs
+++ src/borrow_pcg/action/executed_actions.rs
@@ -4,25 +4,21 @@ use crate::borrow_pcg::borrows_visitor::BorrowPCGActions;
 #[derive(Debug)]
 pub(crate) struct ExecutedActions<'tcx> {
     actions: BorrowPCGActions<'tcx>,
-    changed: bool,
 }
 
 impl<'tcx> ExecutedActions<'tcx> {
     pub(crate) fn new() -> Self {
         Self {
             actions: BorrowPCGActions::new(),
-            changed: false,
         }
     }
 
-    pub(crate) fn record(&mut self, action: BorrowPCGAction<'tcx>, changed: bool) {
+    pub(crate) fn record(&mut self, action: BorrowPCGAction<'tcx>) {
         self.actions.push(action);
-        self.changed |= changed;
     }
 
     pub(crate) fn extend(&mut self, actions: ExecutedActions<'tcx>) {
         self.actions.extend(actions.actions);
-        self.changed |= actions.changed;
     }
 
     pub(crate) fn actions(self) -> BorrowPCGActions<'tcx> {
