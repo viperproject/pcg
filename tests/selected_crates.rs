6ed490ef8f9a08c504b130cca535281a2eb07249
Zack Grannan <zgrannan@protonmail.com>
2025-03-10 11:55:31 -0700

    Try allowing more casts

--- tests/selected_crates.rs
+++ tests/selected_crates.rs
@@ -5,10 +5,11 @@ fn test_selected_crates() {
     // Create tmp directory if it doesn't exist
     std::fs::create_dir_all("tmp").unwrap();
 
+    // common::run_on_crate("ascii", "1.1.0", true);
     // common::run_on_crate("cc", "1.2.16", true);
     // common::run_on_crate("crc", "3.2.1", true);
     // common::run_on_crate("futures-util", "0.3.31", false);
-    common::run_on_crate("http", "1.2.0", true);
+    // common::run_on_crate("http", "1.2.0", true);
     // common::run_on_crate("miniz_oxide", "0.8.5", true);
     // common::run_on_crate("proc-macro2", "1.0.93", false);
     // common::run_on_crate("regex-automata", "0.4.9", true);
