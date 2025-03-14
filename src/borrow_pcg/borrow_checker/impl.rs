2a01627f6862f33f9e597e62712d880fbe5f6254
Zack Grannan <zgrannan@protonmail.com>
2025-03-14 16:34:55 -0700

    Polonius fixes

--- src/borrow_pcg/borrow_checker/impl.rs
+++ src/borrow_pcg/borrow_checker/impl.rs
@@ -1,42 +1,98 @@
+extern crate polonius_engine;
+use polonius_engine::Output;
+
 use crate::borrow_pcg::coupling_graph_constructor::BorrowCheckerInterface;
 use crate::borrow_pcg::region_projection::PCGRegion;
 use crate::combined_pcs::PCGNode;
 use crate::rustc_interface::borrowck::{
-    BorrowData, BorrowIndex, BorrowSet, RegionInferenceContext,
+    BorrowData, BorrowIndex, BorrowSet, LocationTable, RegionInferenceContext,
+    PoloniusOutput,
 };
 use crate::rustc_interface::data_structures::fx::FxIndexMap;
 use crate::rustc_interface::dataflow::compute_fixpoint;
 use crate::rustc_interface::middle::mir::{self, Location};
+use crate::rustc_interface::middle::ty;
 use crate::rustc_interface::mir_dataflow::{impls::MaybeLiveLocals, ResultsCursor};
 use crate::utils::maybe_remote::MaybeRemotePlace;
-use crate::utils::PlaceRepacker;
+use crate::utils::{HasPlace, PlaceRepacker};
+use crate::BodyAndBorrows;
 use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
 
 #[derive(Clone)]
-pub(crate) struct BorrowCheckerImpl<'mir, 'tcx> {
+pub struct PoloniusBorrowChecker<'mir, 'tcx> {
+    location_table: &'mir LocationTable,
+    repacker: PlaceRepacker<'mir, 'tcx>,
+    output_facts: Rc<PoloniusOutput>,
+    region_cx: &'mir RegionInferenceContext<'tcx>,
+    borrows: &'mir BorrowSet<'tcx>,
+}
+
+impl<'mir, 'tcx> BorrowCheckerInterface<'mir, 'tcx> for PoloniusBorrowChecker<'mir, 'tcx> {
+    fn new(tcx: ty::TyCtxt<'tcx>, body: &'mir impl BodyAndBorrows<'tcx>) -> Self {
+        let location_table = body.location_table();
+        let repacker = PlaceRepacker::new(body.body(), tcx);
+        let output_facts = Output::compute(
+            body.input_facts(),
+            polonius_engine::Algorithm::DatafrogOpt,
+            true,
+        );
+        let region_cx = body.region_inference_context();
+        let borrows = body.borrow_set();
+        Self {
+            location_table,
+            repacker,
+            output_facts: Rc::new(output_facts),
+            region_cx,
+            borrows,
+        }
+    }
+    fn is_live(&self, node: PCGNode<'tcx>, location: Location) -> bool {
+        let regions: Vec<_> = match node {
+            PCGNode::Place(place) => place.regions(self.repacker).into_iter().collect(),
+            PCGNode::RegionProjection(region_projection) => {
+                if let Some(place) = region_projection.base().as_local_place() {
+                    let mut regions: Vec<_> =
+                        place.place().regions(self.repacker).into_iter().collect();
+                    regions.push(region_projection.region(self.repacker));
+                    regions
+                } else {
+                    todo!()
+                }
+            }
+        };
+        regions.iter().any(|region| match region {
+            PCGRegion::RegionVid(region_vid) => self
+                .output_facts
+                .origin_contains_loan_at(self.location_table.start_index(location))
+                .contains_key(&(*region_vid).into()),
+            PCGRegion::ReErased => todo!(),
+            PCGRegion::ReStatic => todo!(),
+            PCGRegion::ReBound(_, _) => todo!(),
+            PCGRegion::ReLateParam(_) => todo!(),
+            PCGRegion::PCGInternalErr => todo!(),
+        })
+    }
+
+    fn outlives(&self, sup: PCGRegion, sub: PCGRegion) -> bool {
+        outlives(self.region_cx, sup, sub)
+    }
+
+    fn twophase_borrow_activations(
+        &self,
+        location: Location,
+    ) -> std::collections::BTreeSet<Location> {
+        twophase_borrow_activations(location, self.borrows)
+    }
+}
+
+#[derive(Clone)]
+pub struct BorrowCheckerImpl<'mir, 'tcx> {
     #[allow(unused)]
     repacker: PlaceRepacker<'mir, 'tcx>,
     cursor: Rc<RefCell<ResultsCursor<'mir, 'tcx, MaybeLiveLocals>>>,
     region_cx: &'mir RegionInferenceContext<'tcx>,
     borrows: &'mir BorrowSet<'tcx>,
-}
-
-impl<'mir, 'tcx> BorrowCheckerImpl<'mir, 'tcx> {
-    pub(crate) fn new(
-        repacker: PlaceRepacker<'mir, 'tcx>,
-        region_cx: &'mir RegionInferenceContext<'tcx>,
-        borrows: &'mir BorrowSet<'tcx>,
-    ) -> Self {
-        let cursor = compute_fixpoint(MaybeLiveLocals, repacker.tcx(), repacker.body())
-            .into_results_cursor(repacker.body());
-        Self {
-            repacker,
-            cursor: Rc::new(RefCell::new(cursor)),
-            region_cx,
-            borrows,
-        }
-    }
 }
 #[rustversion::before(2024-12-14)]
 fn cursor_contains_local(
@@ -54,15 +110,24 @@ fn cursor_contains_local(
     cursor.get().contains(local)
 }
 
-impl<'tcx> BorrowCheckerInterface<'tcx> for BorrowCheckerImpl<'_, 'tcx> {
-    fn outlives(&self, sup: PCGRegion, sub: PCGRegion) -> bool {
-        match (sup, sub) {
-            (PCGRegion::RegionVid(sup), PCGRegion::RegionVid(sub)) => {
-                self.region_cx.eval_outlives(sup, sub)
-            }
-            (PCGRegion::ReStatic, _) => true,
-            _ => false,
+impl<'mir, 'tcx> BorrowCheckerInterface<'mir, 'tcx> for BorrowCheckerImpl<'mir, 'tcx> {
+    fn new(tcx: ty::TyCtxt<'tcx>, body: &'mir impl BodyAndBorrows<'tcx>) -> Self {
+        let repacker = PlaceRepacker::new(body.body(), tcx);
+        let region_cx = body.region_inference_context();
+        let borrows = body.borrow_set();
+        Self {
+            repacker,
+            cursor: Rc::new(RefCell::new(
+                compute_fixpoint(MaybeLiveLocals, tcx, body.body())
+                    .into_results_cursor(body.body()),
+            )),
+            region_cx,
+            borrows,
         }
+    }
+
+    fn outlives(&self, sup: PCGRegion, sub: PCGRegion) -> bool {
+        outlives(self.region_cx, sup, sub)
     }
 
     fn is_live(&self, node: PCGNode<'tcx>, location: Location) -> bool {
@@ -88,15 +153,30 @@ impl<'tcx> BorrowCheckerInterface<'tcx> for BorrowCheckerImpl<'_, 'tcx> {
         &self,
         location: Location,
     ) -> std::collections::BTreeSet<Location> {
-        let activation_map = get_activation_map(self.borrows);
-        if let Some(borrows) = activation_map.get(&location) {
-            borrows
-                .iter()
-                .map(|idx| get_reserve_location(&self.borrows[*idx]))
-                .collect()
-        } else {
-            std::collections::BTreeSet::new()
-        }
+        twophase_borrow_activations(location, self.borrows)
+    }
+}
+
+fn outlives(region_cx: &RegionInferenceContext<'_>, sup: PCGRegion, sub: PCGRegion) -> bool {
+    match (sup, sub) {
+        (PCGRegion::RegionVid(sup), PCGRegion::RegionVid(sub)) => region_cx.eval_outlives(sup, sub),
+        (PCGRegion::ReStatic, _) => true,
+        _ => false,
+    }
+}
+
+fn twophase_borrow_activations(
+    location: Location,
+    borrows: &BorrowSet<'_>,
+) -> std::collections::BTreeSet<Location> {
+    let activation_map = get_activation_map(borrows);
+    if let Some(borrow_idxs) = activation_map.get(&location) {
+        borrow_idxs
+            .iter()
+            .map(|idx| get_reserve_location(&borrows[*idx]))
+            .collect()
+    } else {
+        std::collections::BTreeSet::new()
     }
 }
 
