9beff51e34aa7ac9fa7213efe7881df72c95fa2b
Zack Grannan <zgrannan@protonmail.com>
2025-03-16 14:08:37 -0700

    Graph rendering improvements

---
+++ src/borrow_pcg/graph/materialize.rs
@@ -0,0 +1,52 @@
+use derive_more::From;
+
+use crate::{
+    borrow_pcg::{
+        borrow_pcg_edge::{BorrowPCGEdgeLike, BorrowPCGEdgeRef},
+        edge::kind::BorrowPCGEdgeKind,
+    },
+    combined_pcs::PCGNode,
+    utils::PlaceRepacker,
+};
+
+use super::BorrowsGraph;
+
+#[derive(Clone, Copy)]
+pub(crate) struct AliasEdge<'tcx> {
+    pub(crate) blocked_place: PCGNode<'tcx>,
+    pub(crate) blocking_place: PCGNode<'tcx>,
+}
+
+#[derive(Clone, Copy)]
+pub(crate) enum SyntheticEdge<'tcx> {
+    Alias(AliasEdge<'tcx>),
+}
+
+#[derive(From)]
+pub(crate) enum MaterializedEdge<'tcx, 'graph> {
+    Real(BorrowPCGEdgeRef<'tcx, 'graph>),
+    Synthetic(SyntheticEdge<'tcx>),
+}
+
+impl<'tcx> BorrowsGraph<'tcx> {
+    pub(crate) fn materialized_edges<'graph, 'mir>(
+        &'graph self,
+        repacker: PlaceRepacker<'mir, 'tcx>,
+    ) -> Vec<MaterializedEdge<'tcx, 'graph>> {
+        let mut result = Vec::new();
+        for edge in self.edges() {
+            result.push(edge.into());
+            if let BorrowPCGEdgeKind::Borrow(edge) = edge.kind() {
+                if self.contains(edge.deref_place(repacker), repacker) {
+                    result.push(MaterializedEdge::Synthetic(SyntheticEdge::Alias(
+                        AliasEdge {
+                            blocked_place: edge.blocked_place().into(),
+                            blocking_place: edge.deref_place(repacker).into(),
+                        },
+                    )));
+                }
+            }
+        }
+        result
+    }
+}
--- src/visualization/drawer.rs
+++ src/visualization/drawer.rs
@@ -2,17 +2,15 @@ use std::io::{self};
 
 use crate::visualization::dot_graph::DotGraph;
 
-use super::{
-    Graph, GraphDrawer,
-};
+use super::{Graph, GraphDrawer};
 
 impl<T: io::Write> GraphDrawer<T> {
     pub fn new(out: T) -> Self {
         Self { out }
     }
 
-    pub fn draw(mut self, graph: Graph) -> io::Result<()> {
-       let dot_graph = DotGraph {
+    pub(crate) fn draw(mut self, graph: Graph) -> io::Result<()> {
+        let dot_graph = DotGraph {
             name: "CapabilitySummary".to_string(),
             nodes: graph.nodes.iter().map(|g| g.to_dot_node()).collect(),
             edges: graph.edges.into_iter().map(|e| e.to_dot_edge()).collect(),
