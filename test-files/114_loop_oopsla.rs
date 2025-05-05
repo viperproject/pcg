20cc6f4b2bb0600fd4829f63185740fc500438da
Zack Grannan <zgrannan@protonmail.com>
2025-05-05 12:21:40 -0700

    WIP

--- test-files/114_loop_oopsla.rs
+++ test-files/114_loop_oopsla.rs
@@ -4,9 +4,9 @@ struct List {
 }
 
 fn penultimate_mut<'a>(list: &'a mut List) -> Option<&'a mut u32> {
-    let mut current: &'a mut List = list;
+    let mut current = list;
     let mut prev: Option<&'a mut u32> = None;
-// PCG: bb2[0] pre_operands: Loop(bb1): [list↓'?11] -> [prev↓'?13] under conditions bb1 -> bb2,
+    // PCG: bb1[0] post_main: Loop(bb1): [Remote(_1)] -> [current↓'?11, prev↓'?12] under conditions bb1
 
     while let Some(ref mut tail) = current.tail {
         prev = Some(&mut current.head);
