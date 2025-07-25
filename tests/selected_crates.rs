fc6a7850c6bb1930989a3ef57b5a1c8a5d2a85a3
Zack Grannan <zgrannan@protonmail.com>
2025-07-25 08:03:47 -0700

    Skip super long test

--- tests/selected_crates.rs
+++ tests/selected_crates.rs
@@ -731,17 +731,17 @@ fn test_selected_crates() {
         },
     );
 
-    common::ensure_successful_run_on_crate(
-        "brotli-decompressor",
-        "4.0.2",
-        Some("2025-03-13"),
-        common::RunOnCrateOptions::RunPCG {
-            target: common::Target::Debug,
-            validity_checks: true,
-            function: Some("<MemPool<'a, T> as core::default::Default>::default"),
-            extra_env_vars: vec![],
-        },
-    );
+    // common::ensure_successful_run_on_crate(
+    //     "brotli-decompressor",
+    //     "4.0.2",
+    //     Some("2025-03-13"),
+    //     common::RunOnCrateOptions::RunPCG {
+    //         target: common::Target::Debug,
+    //         validity_checks: true,
+    //         function: Some("<MemPool<'a, T> as core::default::Default>::default"),
+    //         extra_env_vars: vec![],
+    //     },
+    // );
 
     common::ensure_successful_run_on_crate(
         "predicates-tree",
