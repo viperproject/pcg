97614342592b743744c37e7e5c1568acf72e53db
Zack Grannan <zgrannan@protonmail.com>
2025-07-24 23:53:22 -0700

    Update tests

--- tests/selected_crates.rs
+++ tests/selected_crates.rs
@@ -62,31 +62,30 @@ fn test_selected_crates() {
         },
     );
 
-    // 159 basic blocks TODO
-    // common::ensure_successful_run_on_crate(
-    //     "serde_yaml",
-    //     "0.9.34+deprecated",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //         function: Some("loader::Loader::<'input>::next_document"),
-    //         extra_env_vars: vec![],
-    //     },
-    // );
+    // 159 basic blocks
+    common::ensure_successful_run_on_crate(
+        "serde_yaml",
+        "0.9.34+deprecated",
+        Some("2025-03-13"),
+        common::RunOnCrateOptions::RunPCG {
+            target: common::Target::Debug,
+            validity_checks: true,
+            function: Some("loader::Loader::<'input>::next_document"),
+            extra_env_vars: vec![],
+        },
+    );
 
-    // 45 blocks TODO
-    // common::ensure_successful_run_on_crate("ring",
-    //     "0.17.14",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //         function: Some("ec::suite_b::ops::p384::p384_scalar_inv_to_mont"),
-    //         extra_env_vars: vec![],
-    //     },
-    // );
-    // return;
+    // 45 blocks
+    common::ensure_successful_run_on_crate("ring",
+        "0.17.14",
+        Some("2025-03-13"),
+        common::RunOnCrateOptions::RunPCG {
+            target: common::Target::Debug,
+            validity_checks: true,
+            function: Some("ec::suite_b::ops::p384::p384_scalar_inv_to_mont"),
+            extra_env_vars: vec![],
+        },
+    );
 
     // 20 basic blocks
     common::ensure_successful_run_on_crate(
@@ -115,17 +114,17 @@ fn test_selected_crates() {
     );
 
     // 23 basic blocks
-    // common::ensure_successful_run_on_crate(
-    //     "prost-build",
-    //     "0.13.5",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //         function: Some("code_generator::CodeGenerator::<'_, 'b>::append_type_attributes"),
-    //         extra_env_vars: vec![],
-    //     },
-    // );
+    common::ensure_successful_run_on_crate(
+        "prost-build",
+        "0.13.5",
+        Some("2025-03-13"),
+        common::RunOnCrateOptions::RunPCG {
+            target: common::Target::Debug,
+            validity_checks: true,
+            function: Some("code_generator::CodeGenerator::<'_, 'b>::append_type_attributes"),
+            extra_env_vars: vec![],
+        },
+    );
 
     // cycles, <= 20 basic blocks, <= 30 nodes
     common::ensure_successful_run_on_crate(
@@ -668,21 +667,6 @@ fn test_selected_crates() {
         },
     );
 
-    // common::ensure_successful_run_on_crate(
-    //     "bindgen",
-    //     "0.71.1",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: false,
-    //         function: None,
-    //         extra_env_vars: vec![(
-    //             "PCG_SKIP_FUNCTION".to_string(),
-    //             "<ir::comp::CompInfo as codegen::CodeGenerator>::codegen".to_string(),
-    //         )],
-    //     },
-    // );
-
     // Polonius also works, checking variable liveness alone isn't sufficient
     // for determining if a lifetime projection is live at a point.
     #[cfg(feature = "custom-rust-toolchain")]
@@ -746,17 +730,17 @@ fn test_selected_crates() {
         },
     );
 
-    // common::ensure_successful_run_on_crate(
-    //     "brotli-decompressor",
-    //     "4.0.2",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //         function: Some("<MemPool<'a, T> as core::default::Default>::default"),
-    //         extra_env_vars: vec![],
-    //     },
-    // );
+    common::ensure_successful_run_on_crate(
+        "brotli-decompressor",
+        "4.0.2",
+        Some("2025-03-13"),
+        common::RunOnCrateOptions::RunPCG {
+            target: common::Target::Debug,
+            validity_checks: true,
+            function: Some("<MemPool<'a, T> as core::default::Default>::default"),
+            extra_env_vars: vec![],
+        },
+    );
 
     common::ensure_successful_run_on_crate(
         "predicates-tree",
@@ -781,18 +765,6 @@ fn test_selected_crates() {
             extra_env_vars: vec![],
         },
     );
-
-    // common::ensure_successful_run_on_crate(
-    //     "encoding_rs",
-    //     "0.8.35",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //         function: None,
-    //         extra_env_vars: vec![],
-    //     },
-    // );
 
     common::ensure_successful_run_on_crate(
         "encoding_rs",
@@ -829,18 +801,6 @@ fn test_selected_crates() {
             extra_env_vars: vec![],
         },
     );
-
-    // common::ensure_successful_run_on_crate(
-    //     "encoding_rs",
-    //     "0.8.35",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: false,
-    //         function: None,
-    //         extra_env_vars: vec![],
-    //     },
-    // );
 
     common::ensure_successful_run_on_crate(
         "serde_derive",
@@ -890,108 +850,5 @@ fn test_selected_crates() {
         },
     );
 
-    // common::ensure_successful_run_on_crate("ascii", "1.1.0", true);
-    // common::ensure_successful_run_on_crate("cc", "1.2.16", true);
-    // common::ensure_successful_run_on_crate("crc", "3.2.1", true);
-    // common::ensure_successful_run_on_crate("futures-util", "0.3.31", false);
-    // common::ensure_successful_run_on_crate("gimli", "0.31.1", false);
-    // common::ensure_successful_run_on_crate(
-    //     "hashbrown",
-    //     "0.15.2",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: true,
-    //     },
-    // );
-    // common::ensure_successful_run_on_crate("http", "1.2.0", true);
-    // common::ensure_successful_run_on_crate("miniz_oxide", "0.8.5", true);
-    // common::ensure_successful_run_on_crate("num-conv", "0.1.0", true);
-    // common::ensure_successful_run_on_crate("num_enum", "0.7.3", true);
-    // common::ensure_successful_run_on_crate("proc-macro2", "1.0.93", false);
-    // common::ensure_successful_run_on_crate("radium", "1.1.0", true);
-    // common::ensure_successful_run_on_crate("regex-automata", "0.4.9", true);
-    // common::ensure_successful_run_on_crate("ring", "0.17.3", true);
-    // common::ensure_successful_run_on_crate("serde_with", "3.12.0", true);
-    // common::ensure_successful_run_on_crate("tap", "1.0.1", false);
 
-    // We should test this consistently because it's a good loop test
-    // common::ensure_successful_run_on_crate(
-    //     "tinytemplate",
-    //     "1.2.1",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: false,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "regex-syntax",
-    //     "0.8.5",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Debug,
-    //         validity_checks: true,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "crossbeam-deque",
-    //     "0.8.6",
-    //     None,
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: false,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "itertools",
-    //     "0.14.0",
-    //     None,
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: false,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "opentelemetry",
-    //     "0.28.0",
-    //     None,
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: false,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "syn",
-    //     "2.0.100",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: true,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate(
-    //     "cfg-if",
-    //     "1.0.0",
-    //     Some("2025-03-13"),
-    //     common::RunOnCrateOptions::RunPCG {
-    //         target: common::Target::Release,
-    //         validity_checks: false,
-    //     },
-    // );
-
-    // common::ensure_successful_run_on_crate("toml_edit", "0.22.23", false);
-    // common::ensure_successful_run_on_crate("tonic", "0.12.3", true);
-    // common::ensure_successful_run_on_crate("wasm-bindgen-backend", "0.2.100", true);
-    // common::ensure_successful_run_on_crate("zerovec-derive", "0.10.3", Some("2025-03-13"), true, false);
-    // common::ensure_successful_run_on_crate("zerocopy-derive", "0.8.23", Some("2025-03-13"), common::RunOnCrateOptions::RunPCG {
-    //     target: common::Target::Debug,
-    //     validity_checks: false,
-    // });
 }
